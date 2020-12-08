7aa5386b74d8a7f3f5424ec3af8b19c45bd70c0d
darkwizard242 <am900820@gmail.com>
2020-12-08 09:51:45 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE_BINARY = '/usr/local/bin/tfsec'
+
+
 def test_tfsec_binary_exists(host):
-    assert host.file('/usr/local/bin/tfsec').exists
+    """
+    Tests if tfsec binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_tfsec_binary_file(host):
-    assert host.file('/usr/local/bin/tfsec').is_file
+    """
+    Tests if tfsec binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_tfsec_binary_which(host):
-    assert host.check_output('which tfsec') == '/usr/local/bin/tfsec'
+    """
+    Tests the output to confirm tfsec's binary location.
+    """
+    assert host.check_output('which tfsec') == PACKAGE_BINARY
